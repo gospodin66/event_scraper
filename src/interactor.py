e5c523eacd4ea14d1fc4bd61c903d5483093e3c8
gospodin66 <borna986@gmail.com>
2024-10-29 18:36:33 +0100

    1.0.0

--- src/interactor.py
+++ src/interactor.py
@@ -52,45 +52,35 @@ class Interactor:
 
     def follow_links(self, pages: Dict[str, str]) -> dict:
         """
-        Follows links crafted by page name and fb events URL.
+        Follows links crafted by event host name and fb events URL.
         Extracts latest events from the events page.
         """
         events = {}
         events_final = {}
-
         for k, p in pages.items():
-
             events_final[k] = []
             events['links'] = []
             events['titles'] = []
-
             page_event_url = f'https://facebook.com/{p}/events'
-            
             self.driver.get(page_event_url)
             self.logger.info(f"Searching for events on {k} event page {page_event_url}")
             time.sleep(WAIT_TIMEOUT)
-
             try:
                 links = self.driver.find_elements(By.XPATH, "//a[@role='link']")
-
                 for link in links:
                     l = link.get_attribute('href')
                     if l in events['links']:
                         continue
-                
                     if 'event' in l:
                         events['links'].append(l)
-
                         try:
                             event = link.find_element(By.XPATH, "./ancestor::div/following-sibling::div")
                         except Exception as e:
                             print(f"Exception raised for event {event}: Unable to find following <div> {e}") 
                             continue
-                            
                         if hasattr(event, 'text'):
                             if event.text:
                                 events['titles'].append(event.text)
-
                 print("\n")
                 for link, title in zip(events['links'], events['titles']):
                     if title.startswith(('Mon','Tue','Wed','Thu','Fri','Sat','Sun')):
@@ -98,15 +88,12 @@ class Interactor:
                         print(ev)
                         events_final[k].append(ev)
                 print("\n")
-
             except TimeoutException as e:
                 ex = '{} raised on follow_links() '.format(e.__class__.__name__)
                 self.logger.error(ex)
             except StaleElementReferenceException as e:
                 ex = '{} raised on follow_links() '.format(e.__class__.__name__)
                 self.logger.error(ex)
-
         self.logger.info("Closing browser.")
         self.driver.quit()
-
         return events_final
