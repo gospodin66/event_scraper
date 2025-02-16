2499dcfa6999fea88926db1d8621ab167b59f6af
cheki <borna986@gmail.com>
2025-02-16 20:19:19 +0100

    add VPN check

--- src/init.py
+++ src/init.py
@@ -1,11 +1,50 @@
 from common import convert_time
-import logging
-import time
+from logging import getLogger
+from time import time
 from scraper import Scraper
+from subprocess import run as subprocess_run, SubprocessError
+from platform import system
+
+logger = getLogger(__name__)
+
+def is_behind_vpn() -> bool:
+    """
+    Check if the user is behind a VPN by checking if gateway IP address is assigned. 
+    If the gateway IP address is assigned, the user is behind a VPN. 
+    Otherwise, the user is not behind a VPN. Supported on Windows OS only.
+    """
+    try:
+        if system() == "Windows":
+            result = subprocess_run(['ipconfig'], capture_output=True, text=True)
+            lines = result.stdout.splitlines()
+
+            for i, line in enumerate(lines):
+                if "NordLynx" in line:
+                    vpn_info = lines[i:i+7]
+                    break
+
+            if not vpn_info:
+                logger.error("VPN interface not found")
+                return False
+                
+            gateway = vpn_info[-1].strip().split(':')[-1].strip()
+            return True if gateway else False
+        
+        else:
+            logger.warning("Linux OS VPN check not supported - skipping")
+            return True
+
+    except SubprocessError as e:
+        logger.error(f"Error checking VPN status: {e}")
+        return False
 
 def main():
-    logger = logging.getLogger(__name__)
-    start = time.time()
+    start = time()
+
+    if not is_behind_vpn():
+        logger.error("Error: Not behind a VPN. Please connect to a VPN and try again.")
+        return 1
+
     s = Scraper()
 
     try:
@@ -16,7 +55,7 @@ def main():
         logger.warning("Keyboard interrupt.")
         return 1
 
-    logger.info(f"Time: {convert_time(time.time() - start)}")
+    logger.info(f"Time: {convert_time(time() - start)}")
     return 0
 
 if __name__ == '__main__':
